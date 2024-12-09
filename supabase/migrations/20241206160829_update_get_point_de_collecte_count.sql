1035485da0d985b206a842f5b824b1604c1005ce
Benoit Guigal <benoit.guigal@protonmail.com>
2024-12-09 17:43:28 +0100

    reformat sql file

--- supabase/migrations/20241206160829_update_get_point_de_collecte_count.sql
+++ supabase/migrations/20241206160829_update_get_point_de_collecte_count.sql
@@ -1,19 +1,24 @@
-set
-  check_function_bodies = off;
+set check_function_bodies = off;
 
-CREATE
-OR REPLACE FUNCTION public.get_total_bouteilles_collecte() RETURNS integer LANGUAGE sql SECURITY DEFINER AS $ function $
-SELECT
-  SUM(collecte_nb_bouteilles) --- lavage à façon
-  + 23500 as "sum"
+CREATE OR REPLACE FUNCTION public.get_total_bouteilles_collecte()
+ RETURNS integer
+ LANGUAGE sql
+ SECURITY DEFINER
+AS $function$SELECT
+  SUM(collecte_nb_bouteilles) 
+  --- lavage à façon
+  + 23500 
+  as "sum"
 FROM
   collecte as c
   LEFT JOIN point_de_collecte AS pc ON c.point_de_collecte_id = pc.id
   LEFT JOIN tournee AS t on c.tournee_id = t.id
 WHERE
   -- Entrepôt Eurocoop
-  c.point_de_collecte_id <> 320 --- Agile en ville
+  c.point_de_collecte_id <> 320 
+  --- Agile en ville
   AND t.transporteur_id <> 3
-  AND t.date < CURRENT_DATE;
+  AND t.date < CURRENT_DATE;$function$
+;
 
-$ function $;
+
