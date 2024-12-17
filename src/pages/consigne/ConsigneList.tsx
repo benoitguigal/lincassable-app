e0e867e43a3e19885378bc517fa7974f5ccd2ce3
Benoit Guigal <benoit.guigal@protonmail.com>
2024-12-17 21:08:39 +0100

    add consigne list

--- src/pages/consigne/ConsigneList.tsx
+++ src/pages/consigne/ConsigneList.tsx
@@ -1,18 +1,15 @@
-import { useTable } from "@refinedev/antd";
 import { IResourceComponentsProps } from "@refinedev/core";
-import { Consigne } from "../../types";
 
 const ConsigneList: React.FC<IResourceComponentsProps> = () => {
-  const { tableProps, tableQueryResult } = useTable<Consigne>({
-    syncWithLocation: true,
-    pagination: { mode: "server", pageSize: 15 },
-    sorters: {
-      mode: "server",
-      initial: [{ field: "date", order: "desc" }],
-    },
-  });
-
-  return <div>Liste des consignes</div>;
+  return (
+    <iframe
+      src="https://statistiques.lincassable.com/public/dashboard/a74d0942-cce8-485a-94a8-2c115632e284"
+      frameBorder="0"
+      width="100%"
+      height="800"
+      allowTransparency
+    ></iframe>
+  );
 };
 
 export default ConsigneList;
