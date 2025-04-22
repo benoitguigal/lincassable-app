801ecf0402cb3f217d4db38a8b21303f75111f6d
Benoit Guigal <benoit.guigal@protonmail.com>
2025-04-22 13:48:15 +0200

    feat(stock): ajoute une section pour visualiser les mouvements de contenant depuis le dernier inventaire

--- src/pages/PrevisionList.tsx
+++ src/pages/PrevisionList.tsx
@@ -68,15 +68,6 @@ const PrevisionList: React.FC<IResourceComponentsProps> = () => {
       {view === "map" && <PrevisionMap />}
     </List>
   );
-  // return (
-  //   <iframe
-  //     src="https://statistiques.lincassable.com/public/question/fc2e43cf-f1a8-4ee5-bc56-6f8801118355"
-  //     frameBorder="0"
-  //     width="100%"
-  //     height="800"
-  //     allowTransparency
-  //   ></iframe>
-  // );
 };
 
 export default PrevisionList;
--- src/pages/collecte/CollecteEdit.tsx
+++ src/pages/collecte/CollecteEdit.tsx
@@ -2,7 +2,7 @@ import { IResourceComponentsProps } from "@refinedev/core";
 import CollecteFormWrapper from "../../components/collecte/CollecteFormWrapper";
 
 const CollecteEdit: React.FC<IResourceComponentsProps> = () => {
-  return <CollecteFormWrapper action="create" />;
+  return <CollecteFormWrapper action="edit" />;
 };
 
 export default CollecteEdit;
